215f03f253ed0e8b50d52732424f6d101ea5e2a2
Zach Taylor <taylorzr@gmail.com>
2017-07-02 14:44:05 -0500

    Move selecting application to function

--- src/main.rs
+++ src/main.rs
@@ -13,22 +13,22 @@ fn parse_input() -> String {
     }
 }
 
-fn build_command(path_string: String) -> Command {
-    let application = {
-        let path = std::path::Path::new(&path_string);
+fn select_application(path_string: &String) -> &'static str {
+    let path = std::path::Path::new(&path_string);
 
-        if path.is_dir() {
-            writeln!(&mut std::io::stderr(), "Contents of directory '{}':", path_string).unwrap();
-            "ls"
-        } else if path.is_file() {
-            writeln!(&mut std::io::stderr(), "Contents of file '{}':", path_string).unwrap();
-            "cat"
-        } else {
-            writeln!(&mut std::io::stderr(), "show: {}: No such file or directory", path_string).unwrap();
-            process::exit(1);
-        }
-    };
+    if path.is_dir() {
+        writeln!(&mut std::io::stderr(), "Contents of directory '{}':", path_string).unwrap();
+        "ls"
+    } else if path.is_file() {
+        writeln!(&mut std::io::stderr(), "Contents of file '{}':", path_string).unwrap();
+        "cat"
+    } else {
+        writeln!(&mut std::io::stderr(), "show: {}: No such file or directory", path_string).unwrap();
+        process::exit(1);
+    }
+}
 
+fn build_command(application: &str, path_string: &String) -> Command {
     let mut command = Command::new(application);
     command.arg(path_string);
     command
@@ -45,8 +45,12 @@ fn run_command(mut command: Command) -> Output {
 }
 
 fn main() {
+    // TODO:
+    // Use ? syntax: https://doc.rust-lang.org/std/result/#the--syntax
+
     let path_string = parse_input();
-    let command     = build_command(path_string);
+    let application = select_application(&path_string);
+    let command     = build_command(&application, &path_string);
     let output      = run_command(command);
 
     println!("{}", String::from_utf8_lossy(&output.stdout));
